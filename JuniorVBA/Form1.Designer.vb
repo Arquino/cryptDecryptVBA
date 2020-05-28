a5695e666e203de75e6e201c525b75564495d669
Arctroid1 <arctroidrobotmink@gmail.com>
2020-05-28 23:27:57 +0200

    initial commit

---
+++ JuniorVBA/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(37, 29)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(218, 119)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(37, 155)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 52)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "crypter"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(166, 154)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(88, 53)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "decrypter"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(424, 253)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ JuniorVBA/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim var As String
+        var = TextBox1.Text'ici j'attribut à la variable *var* la valeur de la textBox appelé *saisie* plus haut'
+
+        'puis je commence la convertion cryptage'
+        var = var.Replace(" ", "60")  'pour convertir l'espace en chiffre selon le tableau k tu as envoyé'
+        var = var.Replace("b", "48")
+        var = var.Replace("o", "50")
+        var = var.Replace("n", "40")
+        var = var.Replace("j", "42")
+        var = var.Replace("o", "50")
+        var = var.Replace("u", "26")
+        var = var.Replace("r", "36")
+        TextBox1.Text = var
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim var As String
+        var = TextBox1.Text
+        'ici j'attribut à la variable *var* la valeur de la textBox appelé *saisie* plus haut'
+
+        'puis je commence la convertion decryptage'
+        var = var.Replace("60", " ")  'pour convertir 60 en espace selon le tableau k tu as envoyé'
+        var = var.Replace("48", "b")
+        var = var.Replace("50", "o")
+        var = var.Replace("40", "n")
+        var = var.Replace("42", "j")
+        var = var.Replace("50", "o")
+        var = var.Replace("26", "u")
+        var = var.Replace("36", "r")
+        TextBox1.Text = var
+    End Sub
+End Class
